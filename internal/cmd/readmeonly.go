d5414490f24b5e9fa31d5586b79003cac3240464
Gafur <sharipovgafur121@gmail.com>
2025-04-25 23:57:17 +0500

    updated the logic readmeonly

--- internal/cmd/readmeonly.go
+++ internal/cmd/readmeonly.go
@@ -5,6 +5,7 @@ import (
 	"gitgeist/internal/git"
 	"github.com/urfave/cli/v2"
 	"log"
+	"os/exec"
 	"strings"
 )
 
@@ -28,7 +29,27 @@ var readmeonly = &cli.Command{
 		}
 		outputString := string(output)
 		result := strings.Split(outputString, "\n")
-		fmt.Println(result)
+		fmt.Println("📝 Коммиты, изменяющие только README.md\n")
+		for _, commitHash := range result {
+			if commitHash == "" {
+				continue
+			}
+
+			cmd := exec.Command("git", "show", "--name-only", "--pretty=", commitHash)
+			out, err := cmd.Output()
+			if err != nil {
+				fmt.Println("Ошибка при выполнении git show:", err)
+				continue
+			}
+
+			files := strings.Split(strings.TrimSpace(string(out)), "\n")
+
+			if len(files) == 1 && strings.ToLower(files[0]) == "readme.md" {
+				fmt.Printf("🔸 `%s` — ⚠️ **Лентяй засечён!** Коммит затронул только `README.md`\n", commitHash)
+			} else {
+				fmt.Printf("✅ `%s` — Внесены **реальные изменения**. Уважаем ✨\n", commitHash)
+			}
+		}
 		return nil
 	},
 }
